9fc81a24ab24eb8159ee9a41b26110c77e34c778
bala Nimmala <nimmalabala00@gmail.com>
2026-03-15 19:15:20 +0000

    Edit script.groovy

--- script.groovy
+++ script.groovy
@@ -19,9 +19,7 @@ def buildImage() {
 
         sh 'docker build -t nimmalabala00/demo-app:jma-3.0 .'
 
-        sh '''
-        echo $PASS | docker login -u $USER --password-stdin
-        '''
+        sh "echo $PASS | docker login -u $USER --password-stdin"
 
         sh 'docker push nimmalabala00/demo-app:jma-3.0'
     }
