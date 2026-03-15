5444be501107dacd8410bf71978bb53493b9eaa6
bala Nimmala <nimmalabala00@gmail.com>
2026-03-15 19:08:13 +0000

    Edit script.groovy

--- script.groovy
+++ script.groovy
@@ -1,24 +1,34 @@
-def buildJar() {
-    echo "building the application..."
+def buildApp() {
+    echo "Building the application..."
     sh 'mvn clean package'
 }
 
 def testApp() {
-    echo "running tests..."
+    echo "Running tests..."
     sh 'mvn test'
 }
 
 def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    echo "Building Docker image..."
+
+    withCredentials([usernamePassword(
+        credentialsId: 'docker-hub-repo',
+        passwordVariable: 'PASS',
+        usernameVariable: 'USER'
+    )]) {
+
         sh 'docker build -t nimmalabala00/demo-app:jma-3.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
+
+        sh '''
+        echo $PASS | docker login -u $USER --password-stdin
+        '''
+
         sh 'docker push nimmalabala00/demo-app:jma-3.0'
     }
 }
 
 def deployApp() {
-    echo "deploying application..."
+    echo "Deploying application..."
 }
 
 return this
